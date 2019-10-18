3564c685a95f523d312668d1be64a6f9bf58b543
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-18 12:04:05 -0600

    Add Item class

---
+++ lib/item.rb
@@ -0,0 +1,12 @@
+class Item
+
+  attr_reader :id, :name, :description, :unit_price, :merchant_id
+
+  def initialize(item_hash)
+    @id = item_hash[:id]
+    @name = item_hash[:name]
+    @description = item_hash[:description]
+    @unit_price = item_hash[:unit_price]
+    @merchant_id = item_hash[:merchant_id]
+  end
+end
