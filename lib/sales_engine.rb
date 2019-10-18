dac669d9a64e9e1bb59adcc0a1babe1a4b375d03
David Holtkamp <david.hholtkamp@gmail.com>
2019-10-18 11:52:52 -0600

    File creation and struggles

---
+++ lib/sales_engine.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+class SalesEngine
+
+
+
+  def self.from_csv
+    " "
+
+  end
+  sales_engine = SalesEngine.from_csv({
+  :items     => "./data/items.csv",
+  :merchants => "./data/merchants.csv",
+})
+
+
+
+
+end
