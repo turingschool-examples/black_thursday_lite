338170305b585068709286e562ca47c07f89f82d
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-18 12:02:09 -0600

    Add edited merchant test

--- test/merchants_test.rb
+++ test/merchants_test.rb
@@ -7,7 +7,7 @@ class MerchantsTest < Minitest::Test
 
   def setup
     @merchant_1 = Merchant.new({
-    :id => 12334105,
+    :id => "12334105",
     :name => "Shopin1901",
     :created_at => 2010-12-10,
     :updated_at => 2011-12-04
@@ -19,7 +19,7 @@ class MerchantsTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal 12334105, @merchant_1.id
+    assert_equal "12334105", @merchant_1.id
     assert_equal "Shopin1901", @merchant_1.name
     assert_equal 2010-12-10, @merchant_1.created_at
     assert_equal 2011-12-04, @merchant_1.updated_at
