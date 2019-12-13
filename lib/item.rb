0e19a74124f5a323c8c723435764fd90c03afbd3
David Atkins <atki1080@gmail.com>
2019-12-13 11:48:07 -0700

    Add item

---
+++ lib/item.rb
@@ -0,0 +1,10 @@
+class Item
+  attr_reader :id, :name, :description, :unit_price, :merchant_id
+  def initialize(item_info)
+    @id = item_info[:id]
+    @name = item_info[:name]
+    @description = item_info[:description]
+    @unit_price = item_info[:unit_price]
+    @merchant_id = item_info[:merchant_id]
+  end
+end
