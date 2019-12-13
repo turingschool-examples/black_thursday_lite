dee655e9ef3a0f46febc3c57cc32539a08ef793b
David Atkins <atki1080@gmail.com>
2019-12-13 11:57:25 -0700

    Add sales_engine test

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+  # def test_it_exists
+  #   # ???
+  # end
+  def test_case_name
+
+  end
+end
