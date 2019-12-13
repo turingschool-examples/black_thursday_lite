99356d475ac96f7527f506f4c149dea74405db3b
Fred Rondina <fredrondina96@gmail.com>
2019-12-13 11:50:28 -0700

    created runner sales engine and test

---
+++ lib/sales_engine.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'csv'
+
+class SalesEngine
+attr_reader :data
+  def initialize(data)
+    @data = data
+  end
+
+end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require_relative '../lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+  end
+
+  def test_engine_exists
+    assert_instance_of SalesEngine, @sales_engine
+  end
+end
