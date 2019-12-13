2079f1dfe068d1ad6039f2c7fcba6cde6f25a74c
Nathan Keller <keller.nathan@gmail.com>
2019-12-13 12:41:25 -0700

    Refactor style of sales engine class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,15 +3,15 @@ require 'csv'
 class SalesEngine
   attr_reader :merchants, :items
 
+    def initialize(merchants, items)
+      @merchants = merchants
+      @items = items
+    end
+
+
     def self.from_csv(locations)
       merchant_path = locations[:merchants]
       item_path = locations[:items]
-
-      self.new(item_path, merchant_path)
-    end
-
-    def initialize(items, merchants)
-      @items = items
-      @merchants = merchants
+      self.new(merchant_path, item_path)
     end
 end
