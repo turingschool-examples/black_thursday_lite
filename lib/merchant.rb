474d55760bb73f734383edb367244d181a21d950
Alex Lynch <mlynch5187@gmail.com>
2019-12-13 11:51:26 -0700

    create classes for merchant, sales_engine, along with tests; runner.rb

---
+++ lib/merchant.rb
@@ -0,0 +1,16 @@
+class Merchant
+
+  def initialize()
+
+
+  end
+
+  def id
+
+  end
+
+  def name
+
+  end
+
+end
---
+++ test/merchant_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/merchant'
+require './data/items.csv'
+require './data/merchants.csv'
+
+
+class MerchantTest < MiniTest::Test
+
+  def test_it_exists
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+    assert_instance_of Merchant, merchant
+  end
+
+  def test_it_has_an_id
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+  end
+
+  def test_it_has_a_name
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+
+  end
+
+
+end
