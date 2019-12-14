10875ee1aaeb8a8b6e8c359499d3abbe9930834a
Elom Amouzou <elom1990@yahoo.com>
2019-12-14 12:34:15 -0700

    Created items and merchants methods

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,20 @@
 class SalesEngine
 
+  def initialize(info)
+    @items = info[:items]
+    @merchants = info[:merchants]
+  end
+
   def self.from_csv(info)
+    self.new(info)
+  end
+
+  def items
+    @items
+  end
+
+  def merchants
+    @merchants
   end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,10 +3,21 @@ require 'minitest/pride'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  def test_it_exists
-    sales_engine = SalesEngine.from_csv({
+  def setup
+    @sales_engine = SalesEngine.from_csv({
       :items      => "./data/items.csv",
       :merchants  => "./data/merchants.csv"
       })
+  end
+
+  def test_it_exists
+
+    assert_instance_of SalesEngine, @sales_engine
+  end
+
+  def test_it_has_information
+
+    assert_equal "./data/items.csv", @sales_engine.items
+    assert_equal "./data/merchants.csv", @sales_engine.merchants
   end
 end
