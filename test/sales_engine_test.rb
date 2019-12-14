058a70b1314730f4bd80eacb33a35432c4882ef2
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-14 16:48:50 -0700

    Created sales_engine test

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+require 'csv'
+
+class SalesEngineTest < Minitest::Test
+
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+  end
+end
+
+  def test_it_exists
+  assert_instance_of Salesengine, @salesengine
+  end
