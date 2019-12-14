39f80f0c559d8547845ce10703e8654bcc667a57
Elom Amouzou <elom1990@yahoo.com>
2019-12-14 15:50:32 -0700

    Create merchant iniitalize method

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,9 +4,11 @@ require './lib/merchant'
 
 class MerchantTest < Minitest::Test
   def setup
-
+    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
   end
 
   def test_it_exists
+
+    assert_instance_of Merchant, @merchant
   end
 end
