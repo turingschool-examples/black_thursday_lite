2cd632cfe1a7360f931a2f3f97135cc203322e97
William Meighan <williamdmeighan@gmail.com>
2019-12-16 16:10:21 -0700

    Aurora refactor

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -3,10 +3,10 @@ class Merchantcollection
   attr_reader :merchants
 
   def initialize(csv_file_path)
-    @merchants = all(csv_file_path)
+    @merchants = create_merchants(csv_file_path)
   end
 
-  def all(csv_file_path)
+  def create_merchants(csv_file_path)
     merchant_array = []
     CSV.foreach("#{csv_file_path}", headers: true, header_converters: :symbol) do |row|
       merchant_array << Merchant.new(row)
@@ -23,5 +23,5 @@ class Merchantcollection
       merchant.id == id
     end
   end
-  
+
 end
