f20d93f9bef8459e150d8b3ecc772770868b8dd0
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-16 11:28:25 -0700

    Add information to runner

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,9 +2,12 @@ require_relative './item_collection'
 require_relative './merchant_collection'
 
 class SalesEngine
+  attr_reader :merchants,
+              :items
+
   def initialize(hash_info)
-    @item_collection = ItemCollection.new(hash_info[:items])
-    @merchant_collection = MerchantCollection.new(hash_info[:merchants])
+    @items = ItemCollection.new(hash_info[:items])
+    @merchants = MerchantCollection.new(hash_info[:merchants])
   end
 
   def self.from_csv(sales_info)
--- runner.rb
+++ runner.rb
@@ -4,3 +4,13 @@ sales_engine = SalesEngine.from_csv({
   :items     => "./data/items.csv",
   :merchants => "./data/merchants.csv",
 })
+
+merchant_collection = sales_engine.merchants
+item_collection = sales_engine.items
+merchant = merchant_collection.find(34)
+items = item_collection.where(34)
+
+p merchant_collection
+p item_collection
+p merchant
+p items
