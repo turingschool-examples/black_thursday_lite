b64f24a8b75d9ab057240a8d4e0e0603bf5043aa
David Atkins <atki1080@gmail.com>
2019-12-16 09:24:03 -0700

    Add test cases for find and all

--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -9,11 +9,19 @@ class MerchantCollectionTest < Minitest::Test
     @merchant2 = Merchant.new({id: 2, name: "Michael"})
     @merchant3 = Merchant.new({id: 3, name: "Sarah"})
     @merchants = [@merchant1, @merchant2, @merchant3]
+    @merchant_collection = MerchantCollection.new(@merchants)
   end
 
   def test_it_exists
-    merchant_collection = MerchantCollection.new(@merchants)
+    assert_instance_of MerchantCollection, @merchant_collection
+  end
 
-    assert_instance_of MerchantCollection, merchant_collection
+  def test_it_returns_all_merchants
+    assert_equal @merchants, @merchant_collection.all
+  end
+
+  def test_it_can_find_a_merchant_by_id
+    assert_equal @merchant2, @merchant_collection.find(2)
+    assert_nil @merchant_collection.find(500)
   end
 end
