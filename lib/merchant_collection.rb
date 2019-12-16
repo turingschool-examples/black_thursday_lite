9b7fc0b7b2b0930219addd57235fb1e4e40345aa
David Atkins <atki1080@gmail.com>
2019-12-16 09:24:40 -0700

    Add attr_reader for all, add find method

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -1,5 +1,10 @@
 class MerchantCollection
+  attr_reader :all
   def initialize(merchants)
-    @merchants = merchants
+    @all = merchants
+  end
+
+  def find(id)
+    @all.find { |merchant| merchant.id == id }
   end
 end
