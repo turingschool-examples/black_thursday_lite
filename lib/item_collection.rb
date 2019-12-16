4e6fef40d934f415f1a7b8a4cf121bf407887184
David Atkins <atki1080@gmail.com>
2019-12-16 09:56:34 -0700

    Add where method

--- lib/item_collection.rb
+++ lib/item_collection.rb
@@ -4,4 +4,10 @@ class ItemCollection
   def initialize(items)
     @all = items
   end
+
+  def where(merchant_id)
+    matched_items = @all.find_all { |item| item.merchant_id == merchant_id }
+    matched_items = nil if matched_items.empty?
+    matched_items
+  end
 end
