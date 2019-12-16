fe3af00e449ab1ec06b3b0bd4754d699605238cc
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-16 08:19:22 -0700

    Add logic for merchant_collection

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -1,12 +1,17 @@
 class MerchantCollection
 
   def initialize(merchant_info)
-    
+    # @names = merchant_info[:name]
+    # @id = merchant_info[:id]
   end
 
   def find(merchant_id)
+    # take list of merchants
+    #   merchant_info.find { |merchant| merchant_info[:id] == merchant_id }
+    #     returns nil if the ID does not exist there
   end
 
   def all
+    # print array of merchants from the original list
   end
 end
