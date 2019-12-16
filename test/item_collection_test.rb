e1d11fc7092454e7202088a64a9044c7daa7505f
David Atkins <atki1080@gmail.com>
2019-12-16 11:39:09 -0700

    Add tests for expanded where method

--- test/item_collection_test.rb
+++ test/item_collection_test.rb
@@ -26,6 +26,13 @@ class ItemCollectionTest < Minitest::Test
       :unit_price  => 20099,
       :merchant_id => 45
     })
+    @item4 = Item.new({
+      :id          => 4,
+      :name        => "Pencil",
+      :description => "You can use it to write things in style",
+      :unit_price  => 50,
+      :merchant_id => 45
+    })
     @items = [@item1, @item2, @item3]
     @item_collection = ItemCollection.new(@items)
   end
@@ -39,8 +46,24 @@ class ItemCollectionTest < Minitest::Test
   end
 
   def test_it_returns_all_items_of_a_merchant_id
-    assert_equal [@item1, @item2], @item_collection.where(2)
-    assert_equal [@item3], @item_collection.where(45)
-    assert_nil @item_collection.where(90)
+    assert_equal [@item1, @item2], @item_collection.where({merchant_id: 2})
+    assert_equal [@item3], @item_collection.where({merchant_id: 45})
+    assert_nil @item_collection.where({merchant_id: 90})
+  end
+
+  def test_it_can_search_for_other_things
+    @item_collection2 = ItemCollection.new([@item1, @item2, @item3, @item4])
+    expected = @item_collection2.where({merchant_id: 2})
+
+    assert_equal [@item1, @item2], expected
+
+    expected = @item_collection2.where({name: 'Pencil'})
+
+    assert_equal [@item1, @item4], expected
+
+    expected = @item_collection2.where({price: 50})
+
+    assert_equal [@item4], expected
+
   end
 end
