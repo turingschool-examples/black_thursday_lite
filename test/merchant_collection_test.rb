be93450f8acb1e4df0f06cdf25146822a6d91dc3
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-16 08:31:24 -0700

    Test for attributes in merchant

--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -11,6 +11,11 @@ class MerchantCollectionTest < Minitest::Test
     assert_instance_of MerchantCollection, @merchant_collection
   end
 
+  def test_merchant_collection_can_get_attributes
+    assert_equal 5, @merchant_collection.id
+    assert_equal "Turing School", @merchant_collection.name
+  end
+
   def test_merchant_collection_can_find_all_merchants
     assert_equal nil, @merchant_collection.find(10)
     assert_equal @merchant_collection, @merchant_collection.find(5)
