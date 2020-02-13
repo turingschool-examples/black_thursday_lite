1d268ac3895faf8e7bc50fb17cd05aac68d5a8ee
Tyler Porter <tylerpporter@gmail.com>
2020-02-13 15:42:17 -0700

    Pass test_it_has_attributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,11 +4,25 @@ require './lib/item.rb'
 class ItemTest < Minitest::Test
 
   def setup
-    @new_instance = Item.new
+    @item = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => 1099,
+      :merchant_id => 2
+    })
   end
 
   def test_it_exists
-    assert_instance_of Item, @new_instance
+    assert_instance_of Item, @item
+  end
+
+  def test_it_has_attributes
+    assert_equal 1, @item.id
+    assert_equal "Pencil", @item.name
+    assert_equal "You can use it to write things", @item.description
+    assert_equal 1099, @item.unit_price
+    assert_equal 2, @item.merchant_id 
   end
 
 end
