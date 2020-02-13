43f57b7cb79e0949300203004d00b2c27aa8e12f
Alex Latham <alexlthm@gmail.com>
2020-02-13 15:18:44 -0700

    Rename parameter for init method

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,9 +1,9 @@
 class Merchant
   attr_reader :id, :name
 
-  def initialize(merchant_data)
-    @id = merchant_data[:id]
-    @name = merchant_data[:name]
+  def initialize(merchant_params)
+    @id = merchant_params[:id]
+    @name = merchant_params[:name]
   end
 
 end
