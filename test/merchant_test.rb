d0b52046fba017214ae1ea3348ec2a9ce7360b58
Alex Latham <alexlthm@gmail.com>
2020-02-13 14:34:16 -0700

    Add test files for SalesEngine and Merchant classes

---
+++ test/merchant_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/merchant'
+
+class MerchantTest < Minitest::Test
+
+
+end
---
+++ test/salesengine_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/salesengine'
+
+class SalesEngineTest < Minitest::Test
+
+
+end
