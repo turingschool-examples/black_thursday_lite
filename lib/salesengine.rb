06d17b77ea4be1ea213fb227fa7b05b05ba689aa
Alex Latham <alexlthm@gmail.com>
2020-02-13 15:22:46 -0700

    Refactor beginnings of salesengine class and tests

--- lib/salesengine.rb
+++ lib/salesengine.rb
@@ -1,4 +1,13 @@
 class SalesEngine
 
+  def initialize
+    @items = nil
+    @merchants = nil
+  end
+
+  def self.from_csv(csv_params)
+    @items = csv_params[:items]
+    @merchants = csv_params[:merchants]
+  end
 
 end
--- test/salesengine_test.rb
+++ test/salesengine_test.rb
@@ -4,5 +4,14 @@ require './lib/salesengine'
 
 class SalesEngineTest < Minitest::Test
 
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items    => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+  end
+
+  def test_it_can_exist
+  end
 
 end
