b5c42cf926ed567927e331825b821fdaceb2a41d
Alex Latham <alexlthm@gmail.com>
2020-02-13 14:41:46 -0700

    Add init method for merchant class

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,5 +4,12 @@ require './lib/merchant'
 
 class MerchantTest < Minitest::Test
 
+  def setup
+    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  end
+
+  def test_it_can_exist
+    assert_instance_of Merchant, @merchant
+  end
 
 end
