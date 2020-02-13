c50b065fbf043353dfa31d7bcca1756234373b9d
Ryan Camp <cmpprg@icloud.com>
2020-02-13 15:59:30 -0700

    Create attr_readers for attributes

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,5 +1,8 @@
 class Merchant
+  attr_reader :name, :id
+
   def initialize(**attr)
-    
+    @name = attr[:name]
+    @id = attr[:id]
   end
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -8,4 +8,11 @@ class MerchantTest < Minitest::Test
 
     assert_instance_of Merchant, merchant
   end
+
+  def test_it_can_access_attributes
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal 5, merchant.id
+    assert_equal "Turing School", merchant.name
+  end
 end
