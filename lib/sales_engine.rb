11245b51c3a7fa1041657fa3419b8dc405562285
Mariana Cid <cidmariana5@gmail.com>
2020-02-14 11:31:17 -0700

    test and file created to get collection from csv.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -26,34 +26,4 @@ require_relative 'merchant_collection'
     MerchantCollection.new(@merchant_path)
   end
 
-  def top_item_producing_merchants
-    items_by_merchant = Hash.new(0)
-    items.each do |item|
-      items_by_merchant[item.merchant_id] += 1
-    end
-
-    total_items = items.length
-    total_merchants = merchant_collection.all.length
-    avg_item_for_merchant = total_items / total_merchants.to_f
-
-    squared_differences = items_by_merchant.map do |merchant_id, item_count|
-      (item_count - avg_item_for_merchant)**2
-    end
-
-    avg_squared_difference = squared_differences.sum.to_f / squared_differences.length
-
-    standard_deviation = Math.sqrt(avg_squared_difference)
-
-    top_item_producer_ids = []
-    items_by_merchant.each do |merchant_id, item_count|
-      if item_count > avg_item_for_merchant + (standard_deviation * 3)
-        top_item_producer_ids << merchant_id
-      end
-    end
-
-    top_item_producer_ids.map do |id|
-      merchant = merchant_collection.find(id)
-      merchant.name
-    end
-  end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -6,6 +6,14 @@ require './lib/merchant'
 require './lib/item'
 class SalesEngineTest < Minitest::Test
 
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+
+    end
+
     def test_it_exists
         sales_engine = SalesEngine.from_csv({
         :items     => "./data/items.csv",
@@ -14,4 +22,20 @@ class SalesEngineTest < Minitest::Test
 
         assert SalesEngine, sales_engine
     end
+
+
+  def test_it_has_attributes
+    assert_equal "./data/items.csv", @sales_engine.item_path
+    assert_equal "./data/merchants.csv", @sales_engine.merchant_path
+  end
+
+  def test_it_can_create_an_item_collection
+    assert_instance_of Array, @sales_engine.items
+    assert_equal 1367, @sales_engine.items.length
+    assert_instance_of Item, @sales_engine.items.first
+  end
+
+  def test_it_can_create_a_merchant_collection
+    assert_instance_of MerchantCollection, @sales_engine.merchant_collection
+  end
 end
