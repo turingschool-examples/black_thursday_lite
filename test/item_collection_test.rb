6b5e07894eeedbbcd289334a808155f2fd4a35a1
Andrew Reid <areid.sj@gmail.com>
2020-02-14 10:24:56 -0700

    Cleanup item collection test file

--- test/item_collection_test.rb
+++ test/item_collection_test.rb
@@ -6,28 +6,28 @@ require 'csv'
 
 class ItemCollectionTest < Minitest::Test
 
-def setup
-  @item_collection = ItemCollection.new("./data/items.csv")
-end
-
-def test_it_exists
-  assert_instance_of ItemCollection, @item_collection
-end
-
-def test_it_has_items
-  assert_equal 1367, @item_collection.items.size
-end
-
-def test_it_can_return_all_items
-  assert_equal 1367, @item_collection.all.size
-end
-
-def test_it_can_return_items_by_merchant
-  expected = @item_collection.items.select do |item|
-    item.merchant_id == "12334185"
+  def setup
+    @item_collection = ItemCollection.new("./data/items.csv")
   end
 
-  assert_equal expected, @item_collection.where(12334185)
-end
+  def test_it_exists
+    assert_instance_of ItemCollection, @item_collection
+  end
+
+  def test_it_has_items
+    assert_equal 1367, @item_collection.items.size
+  end
+
+  def test_it_can_return_all_items
+    assert_equal 1367, @item_collection.all.size
+  end
+
+  def test_it_can_return_items_by_merchant
+    expected = @item_collection.items.select do |item|
+      item.merchant_id == "12334185"
+    end
+
+    assert_equal expected, @item_collection.where(12334185)
+  end
 
 end
