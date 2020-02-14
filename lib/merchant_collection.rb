5649bf32242c763aaedfc20f5d2c3934329f3b01
Ryan Camp <cmpprg@icloud.com>
2020-02-14 12:05:08 -0700

    Create collect_merchant, create_merchant_collection, & find methods

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -1,4 +1,6 @@
 require "./lib/merchant"
+require "csv"
+
 class MerchantCollection
   attr_reader :file_path, :merchants
 
@@ -9,5 +11,19 @@ class MerchantCollection
 
   def instantiate_merchant(input)
     Merchant.new(input)
+  end
+
+  def collect_merchant(merchant)
+    @merchants << merchant
+  end
+
+  def create_merchant_collection
+    CSV.foreach(@file_path, headers:true, header_converters: :symbol) do |row|
+      collect_merchant(instantiate_merchant(row))
+    end
+  end
+
+  def find(id)
+    @merchants.find {|merchant| merchant.id == id.to_s} 
   end
 end
--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -5,28 +5,60 @@ require "./lib/merchant"
 
 class MerchantCollectionTest < Minitest::Test
   def test_that_it_exists
-    csv_path = "./data/merchants"
+    csv_path = "./data/merchants.csv"
     merchant_collection = MerchantCollection.new(csv_path)
 
     assert_instance_of MerchantCollection, merchant_collection
   end
 
   def test_that_it_can_read_its_attributes
-    csv_path = "./data/merchants"
+    csv_path = "./data/merchants.csv"
     merchant_collection = MerchantCollection.new(csv_path)
 
-    assert_equal "./data/merchants", merchant_collection.file_path
-    assert_equal [], merchant_collection.merchants
+    assert_equal "./data/merchants.csv", merchant_collection.file_path
+    assert_instance_of Array, merchant_collection.merchants
   end
 
   def test_it_can_create_merchant_items
-    csv_path = "./data/merchants"
+    csv_path = "./data/merchants.csv"
     merchant_collection = MerchantCollection.new(csv_path)
     input = {name:"Ryan", id:31}
+
     assert_instance_of Merchant, merchant_collection.instantiate_merchant(input)
     assert_equal "Ryan", merchant_collection.instantiate_merchant(input).name
     assert_equal 31, merchant_collection.instantiate_merchant(input).id
+  end
 
+  def test_that_it_can_collect_merchants
+    csv_path = "./data/merchants.csv"
+    merchant_collection = MerchantCollection.new(csv_path)
+    input1 = {name:"Ryan", id:31}
+    input2 = {name:"Andy", id:60}
+    merchant1 = merchant_collection.instantiate_merchant(input1)
+    merchant2 = merchant_collection.instantiate_merchant(input2)
+    merchant_collection.collect_merchant(merchant1)
+    merchant_collection.collect_merchant(merchant2)
+
+    assert_equal [merchant1, merchant2], merchant_collection.merchants
+  end
+
+  def test_it_can_instantiate_and_collect_merchants_from_csv_file
+    csv_path = "./data/merchants.csv"
+    merchant_collection = MerchantCollection.new(csv_path)
+    merchant_collection.create_merchant_collection
+
+    assert_instance_of Merchant, merchant_collection.merchants.first
+    assert_instance_of Merchant, merchant_collection.merchants.last
+    assert_equal 475, merchant_collection.merchants.length
+  end
+
+  def test_it_can_find_a_merchant_by_id
+    csv_path = "./data/merchants.csv"
+    merchant_collection = MerchantCollection.new(csv_path)
+    merchant_collection.create_merchant_collection
+
+    assert_equal "12334471", merchant_collection.find(12334471).id
+    assert_equal "Hollipoop", merchant_collection.find(12334471).name
   end
 
 end
