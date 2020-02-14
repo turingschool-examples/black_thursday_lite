67d1915a3e12c4498b3ee885613110f6a20e5e11
Ryan Camp <cmpprg@icloud.com>
2020-02-14 10:20:58 -0700

    Create instantiate merchant method with test

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -1,6 +1,13 @@
+require "./lib/merchant"
 class MerchantCollection
+  attr_reader :file_path, :merchants
 
   def initialize(file_path)
     @file_path = file_path
+    @merchants = []
+  end
+
+  def instantiate_merchant(input)
+    Merchant.new(input)
   end
 end
--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/merchant_collection"
+require "./lib/merchant"
 
 class MerchantCollectionTest < Minitest::Test
   def test_that_it_exists
@@ -9,4 +10,23 @@ class MerchantCollectionTest < Minitest::Test
 
     assert_instance_of MerchantCollection, merchant_collection
   end
+
+  def test_that_it_can_read_its_attributes
+    csv_path = "./data/merchants"
+    merchant_collection = MerchantCollection.new(csv_path)
+
+    assert_equal "./data/merchants", merchant_collection.file_path
+    assert_equal [], merchant_collection.merchants
+  end
+
+  def test_it_can_create_merchant_items
+    csv_path = "./data/merchants"
+    merchant_collection = MerchantCollection.new(csv_path)
+    input = {name:"Ryan", id:31}
+    assert_instance_of Merchant, merchant_collection.instantiate_merchant(input)
+    assert_equal "Ryan", merchant_collection.instantiate_merchant(input).name
+    assert_equal 31, merchant_collection.instantiate_merchant(input).id
+
+  end
+
 end
