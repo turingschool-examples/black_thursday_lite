ce10be76fb2f85aafcf7b1a50b246e8ad83298a9
Anthony Gomes <code@tonygom.es>
2020-02-13 18:27:21 -0700

    Push save

---
+++ lib/sales_engine.rb
@@ -0,0 +1,14 @@
+
+
+class SalesEngine
+  attr_reader :merchants,
+              :items
+
+  def initialize(sales_data)
+
+  end
+
+  def self.from_csv(sales_raw_data)
+    self.new(sales_raw_data)
+  end
+end
