84a2e7de8faf5f29fef7807af854424839290a48
Andrew Reid <areid.sj@gmail.com>
2020-02-14 08:36:27 -0700

    Refactor to use from csv method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -8,11 +8,16 @@ class SalesEngine
   attr_accessor :item_collection,
                 :merchant_collection
 
+
   def initialize(argument)
     @items = argument[:items]
     @merchants = argument[:merchants]
     @item_collection
     @merchant_collection
+  end
+
+  def self.from_csv(argument)
+    self.new(argument)
   end
 
 end
