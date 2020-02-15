94c9b6a26158781d690a9577f5ba3d83a7367ff3
Ryan Camp <cmpprg@icloud.com>
2020-02-15 13:03:34 -0700

    Create SalesEngine class that instantiates thru class method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,3 +1,13 @@
 class SalesEngine
-  
+  attr_reader :items_csv, :merchants_csv
+
+  def initialize(**files)
+    @items_csv = files[:items]
+    @merchants_csv = files[:merchants]
+  end
+
+  def self.from_csv(csv_files)
+    self.new(csv_files)
+  end
+
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -8,4 +8,25 @@ class SalesEngineTest < Minitest::Test
 
     assert_instance_of SalesEngine, sales_engine
   end
+
+  def test_it_can_instantiate_with_class_method_and_attributes
+    sales_engine = SalesEngine.from_csv({
+                                        :items     => "./data/items.csv",
+                                        :merchants => "./data/merchants.csv",
+                                        })
+
+    assert_instance_of SalesEngine, sales_engine
+    assert_equal "./data/items.csv", sales_engine.items_csv
+    assert_equal "./data/merchants.csv", sales_engine.merchants_csv
+  end
+
+  def test_it_can_instantiate_merchant_collection
+    sales_engine = SalesEngine.from_csv({
+                                        :items     => "./data/items.csv",
+                                        :merchants => "./data/merchants.csv",
+                                        })
+    merchant_collection = sales_engine.merchant_collection
+
+    assert_instance_of MerchantCollection, merchant_collection
+  end
 end
