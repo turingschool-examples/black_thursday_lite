2058ee55cfa0555194de39cec56389319ec243c5
Jack Cullen <jpcullen20@gmail.com>
2020-04-02 17:53:53 -0600

    Create instance of sales_engine class and read csv files

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,7 +1,20 @@
+require 'csv'
+require 'pry'
+
 class SalesEngine
 
-  def self.from_csv
-    
+  attr_reader :merchants_array,
+              :merchant_collection
+  def initialize(merchants_array, items_array)
+    @merchants_array = merchants_array
+    @items_array = items_array
+    @merchant_collection = MerchantCollection.new(@merchants_array)
+  end
+
+  def self.from_csv(data)
+    merchants_array = CSV.read(data[:merchants])
+    items_array = CSV.read(data[:items])
+    self.new(merchants_array, items_array)
   end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require "./lib/sales_engine"
+require './lib/sales_engine'
+require 'csv'
 
 
 class SalesEngineTest < Minitest::Test
@@ -13,7 +14,29 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of SalesEngine, @sales_engine
+      assert_instance_of SalesEngine, @sales_engine
   end
 
+  def test_it_loads_merchants_array_attribute
+    header_row = ["id", "name", "created_at", "updated_at"]
+    first_merchant_row = ["12334105", "Shopin1901", "2010-12-10", "2011-12-04"]
+    last_merchant_row = ["12337411", "CJsDecor", "2011-12-09", "2016-01-08"]
+    assert_equal header_row, @sales_engine.merchants_array.first
+    assert_equal first_merchant_row, @sales_engine.merchants_array[1]
+    assert_equal last_merchant_row, @sales_engine.merchants_array.last
+  end
+
+  def test_it_creats_a_merchant_collectiom
+    assert_instance_of MerchantCollection, @sales_engine.merchant_collection
+  end
+
+  def test_it_loads_items_array_from_csv
+    skip
+    assert_equal # expect items array first, last, and header
+  end
+
+  # test we have a merchant collection attribute
+  # @sales_engine.merchant_collection = MerchantCollection.new(CSV array turned into merchant objects)
+  # @sales_engine.merchant_collection.first == instance of Merchant
+  # repeat the process for items
 end
