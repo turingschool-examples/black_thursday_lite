3fc8169932d7e5d1fee7b3f52fa6e60fde63387b
Jack Cullen <jpcullen20@gmail.com>
2020-04-02 17:54:29 -0600

    Add merchant_collection class and test it exists

---
+++ lib/merchant_collection.rb
@@ -0,0 +1,7 @@
+class MerchantCollection
+
+  def initialize(merchants_array)
+    @merchants_array = merchants_array
+  end
+
+end
---
+++ test/merchant_collection_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+require './lib/merchant_collection'
+require 'csv'
+
+class MerchantCollectionTest < Minitest::Test
+
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+    @merchant_collection = @sales_engine.merchant_collection
+  end
+
+  def test_it_exists
+    assert_instance_of MerchantCollection, @merchant_collection
+  end
+end
