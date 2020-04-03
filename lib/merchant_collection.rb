40f80786d060a65b82ec62be229387765a40b1aa
Antonio Jackson <antoniojackson@gmail.com>
2020-04-02 18:03:50 -0600

    Adding find with test

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -4,4 +4,8 @@ class MerchantCollection
   def initialize(merchant_collection)
     @all = merchant_collection
   end
+
+  def find(id)
+    @all.find{ |merchant| merchant.id == id}
+  end
 end
--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -17,4 +17,13 @@ class MerchantCollectionTest < Minitest::Test
     merchant_collection = MerchantCollection.new([merchant1, merchant2])
     assert_equal [merchant1, merchant2], merchant_collection.all
   end
+
+  def test_find
+    merchant1 = Merchant.new({:id => 5, :name => "Turing School"})
+    merchant2 = Merchant.new({:id => 7, :name => "Galvanize"})
+    merchant_collection = MerchantCollection.new([merchant1, merchant2])
+    assert_nil merchant_collection.find(6)
+    assert_equal merchant2, merchant_collection.find(7)
+    assert_equal merchant1, merchant_collection.find(5)
+  end
 end
