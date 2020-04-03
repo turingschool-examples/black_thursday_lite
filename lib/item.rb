610102ac2f1e14239542b98ebcd8c4b4b08fc827
Antonio Jackson <antoniojackson@gmail.com>
2020-04-02 18:25:28 -0600

    Adding Item class with attributes and tests

---
+++ lib/item.rb
@@ -0,0 +1,11 @@
+class Item
+  attr_reader :id, :name, :description, :unit_price, :merchant_id
+
+  def initialize(item)
+    @id = item[:id]
+    @name = item[:name]
+    @description = item[:description]
+    @unit_price = item[:unit_price]
+    @merchant_id = item[:merchant_id]
+  end
+end
