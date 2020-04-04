c71bf5d149d314154cbc0cd5240ce423d8ad16e1
Eric Hale <eric_hale@comcast.net>
2020-04-04 16:12:05 -0600

    Add Merchant class and test

---
+++ test/item_test.rb
@@ -0,0 +1,8 @@
+require './lib/minitest/autorun'
+require './lib/minitest/pride'
+require './lib/items'
+require 'pry'
+
+class ItemsTest < Minitest::Test
+
+end
---
+++ test/sales_enigine_test.rb
@@ -0,0 +1,8 @@
+require './lib/minitest/autorun'
+require './lib/minitest/pride'
+require './lib/sales_engine'
+require 'pry'
+
+class SalesEngineTest < Minitest::Test
+
+end
