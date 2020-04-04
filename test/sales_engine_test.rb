f818b4272b5926b2774165429f35a4f0053470e2
Kiera Allen <kieraaallen@gmail.com>
2020-04-04 14:33:07 -0600

    Add and test instance of new SalesEngine object

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+  end
+
+  def test_it_exists
+    assert_instance_of SalesEngine, @sales_engine
+  end
+end
