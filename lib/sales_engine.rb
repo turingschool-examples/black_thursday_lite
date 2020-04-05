cc744cfdca416f7ec75f1e911efe303f1ff656ef
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-04 20:39:44 -0600

    Add merchant_list method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -10,4 +10,18 @@ class SalesEngine
     merchants = CSV.parse(File.read(data[:merchants]), headers: true)
     SalesEngine.new(items, merchants)
   end
+
+  def merchant_list
+    @merchants.map do|row|
+      merchant = {}
+      merchant[:id] = row['id'].to_i
+      merchant[:name] = row['name']
+      merchant
+    end
+  end
 end
+# sales_engine = SalesEngine.from_csv({
+#   :items     => "./data/items.csv",
+#   :merchants => "./data/merchants.csv",
+# })
+# require './lib/sales_engine'
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,11 +2,20 @@ require "minitest/autorun"
 require './lib/sales_engine'
 require 'pry'
 class SalesEngineTest < Minitest::Test
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+          :items     => "./data/items.csv",
+          :merchants => "./data/merchants.csv",
+        })
+  end
   def test_it_exists
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-    })
-    assert_instance_of SalesEngine, sales_engine
+    assert_instance_of SalesEngine, @sales_engine
+  end
+
+  def test_it_gets_merchant_list
+    expected = {:id => 12334105, :name => "Shopin1901"}
+    assert_equal expected, @sales_engine.merchant_list.first
+    expected = {:id => 12337411, :name => "CJsDecor"}
+    assert_equal expected, @sales_engine.merchant_list.last
   end
 end
