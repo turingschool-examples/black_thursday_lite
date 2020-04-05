d1b81afff9a1580587ca25a342d43645673e2fbf
Derek Borski <derek.borski@gmail.com>
2020-04-04 23:59:00 -0600

    Updates item_collection and merchant_collection methods to parse more specific data from CSV files and changes a few attributes to integers as necessary

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -17,7 +17,9 @@ class SalesEngine
   def self.merchant_collection(merchant_data)
     merchants_array = []
     CSV.foreach(merchant_data, headers: true, header_converters: :symbol) do |row|
-      merchants_array << Merchant.new(row)
+      id = row[:id].to_i
+      name = row[:name]
+      merchants_array << Merchant.new({id: id, name: name})
     end
     merchants_array
   end
@@ -25,7 +27,16 @@ class SalesEngine
   def self.item_collection(item_data)
     items_array = []
     CSV.foreach(item_data, headers: true, header_converters: :symbol) do |row|
-      items_array << Item.new(row)
+      id = row[:id].to_i
+      name = row[:name]
+      description = row[:description]
+      unit_price = row[:unit_price].to_i
+      merchant_id = row[:merchant_id].to_i
+      items_array << Item.new({id: id,
+                               name: name,
+                               description: description,
+                               unit_price: unit_price,
+                               merchant_id: merchant_id})
     end
     items_array
   end
