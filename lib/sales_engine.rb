528d6da24b712ea4baf7316ef347913c9fd09da9
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-05 23:07:10 -0600

    Attempted merchant collection method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,15 +1,26 @@
+require './lib/merchant'
+
 class SalesEngine
   attr_reader :items, :merchants
 
   def initialize(sales_engine_parameter)
     @items = sales_engine_parameter[:items]
     @merchants = sales_engine_parameter[:merchants]
+    @merchant_collection = []
   end
 
   def self.from_csv(data)
-    merchants = data[:merchants]
-    items = data[:items]
+    # merchants = data[:merchants]
+    # items = data[:items]
       SalesEngine.new(data)
+  end
+
+  def merchant_collection
+    merchants = []
+    CSV.foreach(@merchants, headers: true, header_converters: :symbol) do |row|
+      merchants << Merchant.new(row)
+    end
+    MerchantCollection.new(merchants)
   end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/sales_engine'
+require './lib/merchant'
 require 'csv'
 
 class SalesEngineTest < Minitest::Test
@@ -21,6 +22,11 @@ class SalesEngineTest < Minitest::Test
  def test_it_has_attributes
    assert_equal "./data/items.csv", @sales_engine.items
    assert_equal "./data/merchants.csv", @sales_engine.merchants
+   assert_equal [], @sales_engine.merchant_collection
  end
+
+ def test_sales_engine_merchant_collection
+    
+  end
 
 end
