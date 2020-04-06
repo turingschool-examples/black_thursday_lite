067074bed6de18890a0ca7fcf40148894f6cedf4
Kiera Allen <kieraaallen@gmail.com>
2020-04-05 20:36:11 -0600

    Test and add class method from_csv

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,15 @@
 require 'csv'
 
 class SalesEngine
+  attr_reader :merchants_file,
+              :items_file
+
+  def initialize(files)
+    @merchants_file = files[:merchants]
+    @items_file = files[:items]
+  end
+
+  def self.from_csv(files)
+    new(files)
+  end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -4,13 +4,14 @@ require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
 
-  def test_it_exists
-    sales_engine = SalesEngine.new
-    assert_instance_of SalesEngine, sales_engine
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
   end
 
-  # def test_it_includes_merchant_collection
-  #
-  #   assert_instance_of MerchantCollection, @sales_engine.merchant_collection
-  # end
+  def test_it_exists
+    assert_instance_of SalesEngine, @sales_engine
+  end
 end
