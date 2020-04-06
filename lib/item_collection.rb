39a5a37e91921af83c9f70f026d3f68f32c8a166
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-05 20:11:06 -0600

    Fixed typo

--- lib/item_collection.rb
+++ lib/item_collection.rb
@@ -3,12 +3,12 @@ require './lib/item'
 class ItemCollection
   attr_reader :all
 
-  def initalize(all_items)
-    @all_items = all_items
+  def initialize(all_items)
+    @all = all_items
   end
 
   def where(merchant_id)
-    @all_items.find do |item|
+    @all.find do |item|
       item.key(merchant_id)
     end
   end
