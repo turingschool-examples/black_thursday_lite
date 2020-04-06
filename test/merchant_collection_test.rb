8476c1e6483a873248119d76220a1af6b5992c0c
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-05 20:20:36 -0600

    Add test for find method

--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -6,17 +6,30 @@ require "pry"
 
 class MerchantCollectionTest < Minitest::Test
 
-  def test_it_exists
-    sales_engine = SalesEngine.from_csv({
+  def setup
+    @sales_engine = SalesEngine.from_csv({
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv"})
-    merchant_collection = sales_engine.merchant_collection
+  end
+
+  def test_it_exists
+    merchant_collection = @sales_engine.merchant_collection
+  end
+
+  def test_it_can_find
+    assert_nil @sales_engine.merchant_collection.find(34)
+  end
+
+  def test_it_can_create
+   #  `create({name: 'Monster Merchant'})` - This should create a new instance of Merchant with a unique ID, and store it in our Merchant Collection.
+  end
+
+  def test_it_can_update
+    # * `update({id: 34, name: 'New Merchant Name'})` - this should change the name of the Merchant instance to the given value.
+    #
+  end
+
+  def test_it_can_destroy
+    # * `destroy(34)` - This should remove the merchant with the given id from the Merchant Collection.
   end
 end
-
-
-# merchant_collection = sales_engine.merchant_collection
-# merchant = merchant_collection.find(34)
-# # => <Merchant>
-# merchants = merchant_collection.all
-# # => [<Merchant>, <Merchant>...]
