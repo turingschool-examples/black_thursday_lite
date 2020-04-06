b3cc2d746884ddf96d7415a8ef5e8685ebf5c267
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-06 08:25:53 -0600

    update find and where

--- lib/item_collection.rb
+++ lib/item_collection.rb
@@ -4,7 +4,12 @@ class ItemCollection
     @all = items
   end
 
+  def find(id)
+    @items.find { |item| item.merchant_id == item.id}
+  end
+
   def where(merchant_id)
-    @all.find {|item| item.merchant_id == merchant_id}
+    @itesm.find_all do |ite|
+      item.merchant_id == merchant_id
   end
 end
