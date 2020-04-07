eaedada7066e8026fc84d2f485ef17416d19cfff
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-06 19:46:47 -0600

    Refactor to integrate item_collection class as class methods

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,32 @@
+require "csv"
+
 class Item
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.from_csv(file_path)
+    csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
+
+    @@all = csv.map do |row|
+      Item.new(row)
+    end
+  end
+
+  def self.find(id)
+    @@all.find do |item|
+      item.id == id
+    end
+  end
+
+  def self.where(merchant_id)
+    @@all.find_all do |item|
+      item.merchant_id == merchant_id
+    end
+  end
+
   attr_reader :id,
               :name,
               :description,
@@ -6,10 +34,10 @@ class Item
               :merchant_id
 
   def initialize(item_params)
-    @id = item_params[:id]
+    @id = item_params[:id].to_i
     @name = item_params[:name]
     @description = item_params[:description]
-    @unit_price = item_params[:unit_price]
-    @merchant_id = item_params[:merchant_id]
+    @unit_price = item_params[:unit_price].to_i
+    @merchant_id = item_params[:merchant_id].to_i
   end
 end
