e09fcefb10ec41fc33d2917cddbcd4b17c6c5d00
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-06 19:48:14 -0600

    Move csv read to other classes

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,48 +1,31 @@
-require './lib/merchant_collection'
-require './lib/item_collection'
+require './lib/merchant'
+require './lib/item'
 require 'csv'
 require 'pry'
 
 class SalesEngine
 
-  attr_reader :all_items,
-              :all_merchants
-
-  def initialize(items, merchants)
-    @all_items = items
-    @all_merchants = merchants
+  def self.from_csv(file_paths)
+    item_path = file_paths[:items]
+    merchant_path = file_paths[:merchants]
+    SalesEngine.new(item_path, merchant_path)
   end
 
-  def self.from_csv(csv_data)
-    items = csv_data[:items]
-    merchants = csv_data[:merchants]
-    self.new(items, merchants)
+  attr_reader :item_path,
+              :merchant_path
+
+  def initialize(item_path, merchant_path)
+    @item_path = item_path
+    @merchant_path = merchant_path
   end
 
-  def item_collection
-    item_collection = []
-    CSV.foreach(@all_items, headers: true, header_converters: :symbol) do |row|
-      item_params = {}
-      item_params[:id] = row[0]
-      item_params[:name] = row[1]
-      item_params[:description] = row[2]
-      item_params[:unit_price] = row[3]
-      item_params[:merchant_id] = row[4]
-      new_item = Item.new(item_params)
-      item_collection << new_item
-    end
-    ItemCollection.new(item_collection)
+  def items
+    Item.from_csv(@item_path)
+    Item.all
   end
 
-  def merchant_collection
-    merchant_collection = []
-    CSV.foreach(@all_merchants, headers: true, header_converters: :symbol) do |row|
-      merchant_params = {}
-      merchant_params[:id] = row[0]
-      merchant_params[:name] = row[1]
-      new_merchant = Merchant.new(merchant_params)
-      merchant_collection << new_merchant
-    end
-    MerchantCollection.new(merchant_collection)
+  def merchants
+    Merchant.from_csv(@merchant_path)
+    Merchant.all
   end
 end
