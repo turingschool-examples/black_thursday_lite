136cc833d95ab7531d4edaf5cc1fa817ece7cad6
Cam C <ckc.cameron@gmail.com>
2020-05-22 17:58:12 -0600

    Writes all method for MerchantCollection class with passing test; find method gives error for nil:NilClass when calling merchant.id

--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -5,11 +5,30 @@ require './lib/merchants'
 require './lib/merchant_collection'
 
 class MerchantCollectionTest < Minitest::Test
-  def test_it_exists
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-    })
+  def setup
+    @merchant1 = Merchant.new({id: 5, name: "Chipotle"})
+    @merchant2 = Merchant.new({id: 22, name: "Sushi Garden"})
+    @merchant3 = Merchant.new({id: 34, name: "Little Cafe In The City"})
+    @merchants_list = [@merchant1, @merchant2, @merchant3, @merchant4]
+    @merchant_collection = MerchantCollection.new(@merchants_list)
+  end
 
+  def test_it_exists
+
+    assert_instance_of Merchant, @merchant1
+  end
+
+  def test_it_can_list_all_merchants
+
+    assert_equal @merchants_list, @merchant_collection.all
+  end
+
+  def test_it_can_find_merchant_by_id
+
+    assert_equal @merchant1, @merchant_collection.find(5)
+    assert_equal @merchant2, @merchant_collection.find(22)
+    assert_equal @merchant3, @merchant_collection.find(34)
+    assert_nil @merchant_collection.find(50)
+  end
 
 end
