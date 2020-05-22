68f8d94ad1abc368dfabdd256d6ae90b69cc88e0
“Neeru <neeram85@gmail.com>
2020-05-22 10:39:31 -0600

    Creating class files

---
+++ test/salesengine_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/salesengine'
+require 'pry'
+require 'csv'
+
+class SalesEngineTest < Minitest::Test
+  def test_it_exists
+    sales_engine = SalesEngine.from_csv({:items => "./data/items.csv", :merchants => "./data/merchants.csv"})
+    assert_instance_of SalesEngine, sales_engine
+  end
+end
