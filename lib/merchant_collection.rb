9ffbc159706b4158bb202706e7355ca98836aa69
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-24 15:30:33 -0400

    Update merchant collection

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -1,3 +1,19 @@
+require './lib/merchant'
+require 'csv'
+
 class MerchantCollection
-  
+  attr_reader :merchant_file
+
+  def initialize(merchant_file)
+    @merchant_file = merchant_file
+  end
+
+  def all
+    all_merchants = []
+    CSV.read(merchant_file, headers: true).each do |merchant|
+      all_merchants << merchant
+      # require "pry"; binding.pry
+    end
+    all_merchants
+  end
 end
--- test/merchant_collection_test.rb
+++ test/merchant_collection_test.rb
@@ -1,10 +1,29 @@
 require 'minitest/autorun'
-require 'mintiest/pride'
+require 'minitest/pride'
 require './lib/merchant_collection'
 
 class MerchantCollectionTest < MiniTest::Test
+  def setup
+    @merchant_list = './fixtures/merchants_fixture.csv'
+    @merchant_collection = MerchantCollection.new(@merchant_list)
+  end
+
   def test_it_exists
-    
+    assert_instance_of MerchantCollection, @merchant_collection
+  end
+
+  def test_it_can_locate_the_file
+    # skip
+    assert_equal './fixtures/merchants_fixture.csv', @merchant_collection.merchant_file
+  end
+  # eventually we want to test that there is an Array
+
+  def test_it_can_find_all_merchants
+    merchants = @merchant_collection.all
+
+    assert_instance_of Array, merchants
+    assert_equal 4, merchants.count
+    assert_equal true, merchants.all? { |merchant| merchant.class == Merchant }
   end
 end
 =begin
