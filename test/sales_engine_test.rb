80d5ada7645a2e57dc5b31288abed13f00c99a64
Dani Coleman <danicoleman00@gmail.com>
2020-05-25 07:50:13 -0600

    Add sales enginec class and test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -6,16 +6,23 @@ require './lib/sales_engine'
 class SalesEngineTest < Minitest::Test
 
   def test_it_exists
-    sales_engine = SalesEngine.new({:items => "", :merchants})
+    sales_engine = SalesEngine.new({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
 
     assert_instance_of SalesEngine, sales_engine
   end
 
-  def test_it_has_attributes
-    sales_engine = SalesEngine.new({:items => [], :merchants => []})
+  def test_it_has_attribute
+    skip
+    sales_engine = SalesEngine.new({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
 
-    assert_equal [], sales_engine.items
-    assert_equal [], sales_engine.merchants
+    assert_equal "./data/items.csv", sales_engine.items
+    assert_equal "./data/merchants.csv", sales_engine.merchants
   end
 
 end
