9aece5060d9187c3660c8f5177e34dcfd5e0b189
Ryan Laleh <laleh21@yahoo.com>
2020-05-25 08:40:33 -0600

    Add item files

---
+++ lib/item.rb
@@ -0,0 +1,15 @@
+class Item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :merchant_id
+
+  def initialize(info)
+    @id = info[:id]
+    @name = info[:name]
+    @description = info[:description]
+    @unit_price = info[:unit_price]
+    @merchant_id = info[:merchant_id]
+  end
+end
