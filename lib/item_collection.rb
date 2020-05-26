332b0614f4b83a008f863f33a040c2816eca2977
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-25 18:24:23 -0600

    Rework item_collection .where functionality to take a hash as an argument

--- lib/item_collection.rb
+++ lib/item_collection.rb
@@ -16,32 +16,26 @@ class ItemCollection
         description = row["description"]
         unit_price = row["unit_price"].to_i
         merchant_id = row["merchant_id"].to_i
-        items << Item.new({:id => id, :name => name, :description => description, :unit_price => unit_price, :merchant_id => merchant_id})
+        @items << Item.new({:id => id, :name => name, :description => description, :unit_price => unit_price, :merchant_id => merchant_id})
       end
+    end
     @items
   end
 
-  def where(search_merchant_id)
-    all.find_all do |item|
-      item.merchant_id == search_merchant_id
+  def where(hash_to_find)
+    if hash_to_find.keys.include?(:merchant_id)
+      all.find_all do |item|
+        item.merchant_id == hash_to_find[:merchant_id]
+      end
+    elsif hash_to_find.keys.include?(:name)
+      all.find_all do |item|
+        item.name == hash_to_find[:name]
+      end
+    elsif hash_to_find.keys.include?(:price)
+      all.find_all do |item|
+        item.unit_price == hash_to_find[:price]
+      end
     end
   end
 
 end
-
-=begin
-
-Update .where() to do this:
-
-  With this new implementation of where,
-  you should be able to send a key/value pair into the method
-  to return all items where the attribute(key) matches the value given.
-
-  all_pencils = item_collection.where({merchant_id: 34})
-  # => [<Item>, <Item>, <Item>]
-  all_pencils = item_collection.where({name: 'Pencil'})
-  # => [<Item>, <Item>, <Item>]
-  all_fifty_cent_items = item_collection.where({price: 50})
-  # => [<Item>, <Item>, <Item>]
-
-=end
